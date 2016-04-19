b37474e8640911c13646ef95637c1eb67bbea737
Kamil S Jaroň <376090@mail.muni.cz>
2016-04-19 23:11:31 +0200

    BS and AS models

--- scripts/1_sequence_dependent_models.R
+++ scripts/1_sequence_dependent_models.R
@@ -1,31 +1,39 @@
 # ------------SEQ MODELS----------------
-# position and nk
+# BS_model
 BS_model <- lm(formula = logk ~ st + nd + rd, data = retention)
+
+# cv.lm computes a crossvalidation. m is equal to number of mesurements, therefore it is leave-one crossvalidation
 kfCV <- cv.lm(retention, BS_model, m = 27, plotit = F)
 
-capture.output(summary(BS_model), file = './output/1_BS_model_summary.txt')
-capture.output(print('ANOVA'), file = './output/1_BS_model_summary.txt', append = T)
-capture.output(anova(BS_model), file = './output/1_BS_model_summary.txt', append = T)
-capture.output(print('Shapiro-wilk test of residual errors'), append = T, file = './output/1_BS_model_summary.txt')
-capture.output(shapiro.test(BS_model$residuals), file = './output/1_BS_model_summary.txt', append = T)
-capture.output(print('MSE'), append = T, file = './output/1_BS_model_summary.txt')
-capture.output(mean(BS_model$residuals^2), file = './output/1_BS_model_summary.txt', append = T)
-capture.output(print('MSE crossvalidated'), append = T, file = './output/1_BS_model_summary.txt')
-capture.output(mean((kfCV$cvpred - kfCV$logk)^2), file = './output/1_BS_model_summary.txt', append = T)
+BS_summary_file <- './output/1_BS_model_summary.txt'
 
-# position, nk and neighbours
+# BS model statistics
+capture.output(summary(BS_model), file = BS_summary_file)
+capture.output(print('ANOVA'), file = BS_summary_file, append = T)
+capture.output(anova(BS_model), file = BS_summary_file, append = T)
+capture.output(print('Shapiro-wilk test of residual errors'), append = T, file = BS_summary_file)
+capture.output(shapiro.test(BS_model$residuals), file = BS_summary_file, append = T)
+capture.output(print('MSE'), append = T, file = BS_summary_file)
+capture.output(mean(BS_model$residuals^2), file = BS_summary_file, append = T)
+capture.output(print('MSE crossvalidated'), append = T, file = BS_summary_file)
+capture.output(mean((kfCV$cvpred - kfCV$logk)^2), file = BS_summary_file, append = T)
+
+# AS_model
 AS_model <- lm(logk ~ st + nd + rd + st:nd + nd:rd, data = retention)
 kfCV2 <- cv.lm(retention, AS_model, m = 27, plotit = F)
+AS_summary_file <- './output/1_AS_model_summary.txt'
 
-capture.output(summary(AS_model), file = './output/1_AS_model_summary.txt')
-capture.output(print('ANOVA'), file = './output/1_AS_model_summary.txt', append = T)
-capture.output(anova(AS_model), file = './output/1_AS_model_summary.txt', append = T)
-capture.output(print('Shapiro-wilk test of residual errors'), append = T, file = './output/1_AS_model_summary.txt')
-capture.output(shapiro.test(AS_model$residuals), file = './output/1_AS_model_summary.txt', append = T)
-capture.output(print('MSE'), append = T, file = './output/1_AS_model_summary.txt')
-capture.output(mean(AS_model$residuals^2), file = './output/1_AS_model_summary.txt', append = T)
-capture.output(print('MSE crossvalidated'), append = T, file = './output/1_AS_model_summary.txt')
-capture.output(mean((kfCV2$cvpred - kfCV2$logk)^2), file = './output/1_AS_model_summary.txt', append = T)
+# AS model statistics
+capture.output(summary(AS_model), file = AS_summary_file)
+capture.output(print('ANOVA'), file = AS_summary_file, append = T)
+capture.output(anova(AS_model), file = AS_summary_file, append = T)
+capture.output(print('Shapiro-wilk test of residual errors'), append = T, file = AS_summary_file)
+capture.output(shapiro.test(AS_model$residuals), file = AS_summary_file, append = T)
+capture.output(print('MSE'), append = T, file = AS_summary_file)
+capture.output(mean(AS_model$residuals^2), file = AS_summary_file, append = T)
+capture.output(print('MSE crossvalidated'), append = T, file = AS_summary_file)
+capture.output(mean((kfCV2$cvpred - kfCV2$logk)^2), file = AS_summary_file, append = T)
 
+# AS BS submodel test
 capture.output(anova(BS_model,AS_model), file = './output/1_BS_AS_model_ANOVA.txt')
 # at this point, we see a strong dependency of retention factor to sequence of trinucletide.
